571686bb50bc4a35610cf41bd6f4cc197c1b8834
pvgomkar12 <44150909+pvgomkar12@users.noreply.github.com>
2018-10-17 07:42:31 +0530

    Add files via upload

---
+++ first programm.py
@@ -0,0 +1,5 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('hello world')
+hello world
+>>> 
